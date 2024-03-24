a607346806e74e9112bb8a6fe66d1c13d19f74a8
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-03-24 04:02:07 -0700

    [new] sidebar skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Drawer } from '@mui/material';
 import './App.css';
+import { DRAWER_WIDTH } from './constants/dimension';
+import Sidebar from './pages/layout/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Drawer open variant='permanent' sx={{
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: DRAWER_WIDTH }
+      }}>
+        <Sidebar />
+      </Drawer>
     </div>
   );
 }
