53f2f7b9007bc89425fbe252a4183581d291af6c
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-03-24 03:37:21 -0700

    [initial]

---
+++ src/pages/layout/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SidebarHeader from './header'
+
+export default function Sidebar() {
+    return (
+        <div>
+            <SidebarHeader />
+        </div>
+    )
+}
