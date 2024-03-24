e5992a32375f6ed4449b6d80dc5a80f2025b975d
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-03-24 04:15:13 -0700

    [new] logo

--- src/pages/layout/sidebar/header/SidebarHeader.jsx
+++ src/pages/layout/sidebar/header/SidebarHeader.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Logo from '../../../../components/logo'
+import { Box } from '@mui/material'
 
 export default function SidebarHeader() {
   return (
-    <div>SidebarHeader</div>
+    <Box display="flex" justifyContent="center" sx={{mt: 5}}>
+      <Logo />
+    </Box>
   )
 }
